e8216ccfc90dacd100d4f10d1ed0608f2801af9c
khseon7 <gkrtjs5544@gmail.com>
2024-10-28 12:46:47 +0900

    Edit models.py and view.py to store Record audio data passed from React app

--- haksul/study_summary/models.py
+++ haksul/study_summary/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+# 오디오 파일을 저장할 모델
+class AudioRecording(models.Model):
+    uploaded_at=models.DateTimeField(auto_now_add=True)
+    audio_file=models.FileField(upload_to='audio_files/') # 파일 저장 경로
+    
+    def __str__(self):
+        return f"Recording {self.id} at {self.uploaded_at}"
--- haksul/study_summary/views.py
+++ haksul/study_summary/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import AudioRecording
 # Create your views here.
+
+@csrf_exempt
+def upload_audio(request):
+    if request.method=='POST':
+        audio_file=request.FILES.get('audio_file')
+
+        if audio_file:
+            recording=AudioRecording(audio_file=audio_file)
+            recording.save()
+            return JsonResponse({'message':'File uploaded successfully'}, status=200)
+        else:
+            return JsonResponse({'message':'No file provided'}, status=400)
+    return JsonResponse({'error':'Invalid request'},status=400)
