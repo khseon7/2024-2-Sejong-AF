83c62aaf43d0a6665bbe633e2589d4eb6d38a5e7
khseon7 <gkrtjs5544@gmail.com>
2024-10-28 11:35:44 +0900

    Edit react app's title

--- react-haksul/src/App.js
+++ react-haksul/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import VoiceRecorder from './VoiceRecorder';
 
 function App() {
+  useEffect(()=>{
+    document.title='슬기샘';
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <div>
           <h1>음성 녹음 앱</h1>
           <VoiceRecorder />
