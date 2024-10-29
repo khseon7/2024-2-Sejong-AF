e86bbb805afeebc87bf7a863e28b38d13c2d0f71
khseon7 <gkrtjs5544@gmail.com>
2024-10-29 10:26:47 +0900

    edit real mainpage

--- react-haksul/src/pages/mainpage.js
+++ react-haksul/src/pages/mainpage.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import './mainpage.css';
 import { Link } from 'react-router-dom';
 
-function MainPage() {
+function ProjectPage() {
+
     return (
-        <div>
-        <h1>메인 페이지</h1>
-        <p>환영합니다! 음성 녹음 앱을 사용해보세요.</p>
-        <Link to="/record">음성 녹음 페이지로 이동</Link>
+        <div className="project-page">
+            <Link to='/record'>
+                <button className="login-button">로그인</button>
+            </Link>
+            
+            <div className="container">
+            <div className="info">
+                <h1>학습나래</h1>
+                <p>인공지능을 이용한 요약 및 음성 번역</p>
+                <p>이제, 영어 수업도 무섭지 않다 !</p>
+                <p>귀찮았던 필기 요약도 한번에 ?</p>
+                <p>게다가 문제와 피드백 까지 !</p>
+            </div>
+            <div className="logo"></div>
+            </div>
+
+            <footer>
+                <div className="credits">제작 : 최지훈, 김학선, 신재영, 부창오, 이우진, 권경민</div>
+            </footer>
         </div>
-    );
+    ); 
 }
 
-export default MainPage;
+export default ProjectPage;
